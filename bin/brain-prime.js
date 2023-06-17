c674fa5782632a865460a2cd10388d48011eeeba
MiggRabbid <mig26@inbox.ru>
2023-06-17 11:44:47 +0300

    Maked brain-progression

---
+++ bin/brain-prime.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import { getUser, getRandomNumber, isCorrectAnswer} from "../src/computing.js";
+
+const user = getUser();
+console.log(`Answer "yes" if given number is prime. Otherwise answer "no".`);
+
+const isPrimeNumber = () => {
+    const number = getRandomNumber(0.001, 1);
+    console.log(`Question: ${number}`);
+
+    for (let i = 2; i < (number / 2); i += 1) {
+        if (number % i === 0) {
+          return 'no';
+        }
+      }
+      return 'yes';
+}
+
+
+const getAnswer = () => {
+    let i = 0;
+
+    while (i < 3) {
+        let expected = isPrimeNumber();
+        
+        if (isCorrectAnswer(expected) === true ) {
+            i += 1;
+        } else {
+            return `Let's try again, ${user}!`;
+        }
+    }
+
+    return `Congratulations, ${user}!`;
+}
+
+console.log(getAnswer());
